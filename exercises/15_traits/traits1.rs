559618b5e9cb80d39e46c7b67df15021a1946389
AliNAbadian <nagshrizali@gmail.com>
2025-05-16 16:03:32 +0330

    Refactor generics and traits exercises to enhance type handling and implement trait methods

--- exercises/15_traits/traits1.rs
+++ exercises/15_traits/traits1.rs
@@ -6,6 +6,10 @@ trait AppendBar {
 
 impl AppendBar for String {
     // TODO: Implement `AppendBar` for the type `String`.
+    fn append_bar(self) -> String {
+        let result = self.to_string() + "Bar";
+        return result;
+    }
 }
 
 fn main() {
